5621c86cc5ef38754dea7d30b2ccb066da9ac074
Fabien Piuzzi <reefab@demenzia.net>
2013-01-07 15:12:14 +0100

    Moved source files around to use https://github.com/amperka/ino for builds

--- src/runkeeper.h
+++ src/runkeeper.h
@@ -6,8 +6,7 @@
  *      Author: reefab
  */
 
-byte serverIp[] = {
-  74,50,63,142};
+byte serverIp[] = {74,50,63,142};
 #define apiServer "api.runkeeper.com"
 #define apiUri "/fitnessActivities"
 #define json_str1 "{\"type\": \"Cycling\",\"start_time\": \""
@@ -26,8 +25,7 @@ HTTPClient http_client(apiServer, serverIp);
 
 boolean uploadResult(String startTimeStr, unsigned int totalDistance, unsigned long effectiveTime)
 {
-
-    String data; 
+    String data;
     data += json_str1;
     data += startTimeStr;
     data += json_str2;
@@ -39,39 +37,35 @@ boolean uploadResult(String startTimeStr, unsigned int totalDistance, unsigned l
     char apiData[bufSize];
     data.toCharArray(apiData, bufSize);
 
-  http_client_parameter apiHeaders[] = {
-    { 
-      header_str1, accessToken          }
-    ,
-    { 
-      header_str2, header_str3      }
-    ,
-    {
-      NULL, NULL          }
-  };
-  lcd.print(status_inprogress);
-  delay(500);
-  http_client.debug(-1);
-  FILE* result = http_client.postURI(apiUri, NULL, apiData, apiHeaders);
-  int returnCode = http_client.getLastReturnCode();
-  lcd.clear();
-  if (result!=NULL) {
-    http_client.closeStream(result);  // this is very important -- be sure to close the STREAM
-  } 
-  else {
-    lcd.print(status_failure);
-  }
-  if (returnCode==201) {
-    lcd.print(status_data_uploaded);
-    lcd.setCursor(0, 1);
-    lcd.print(status_session_created);
-    return true;
-  } 
-  else {
-    lcd.print(status_error_code);
-    lcd.setCursor(0, 1);
-    lcd.print(returnCode);
-    return false;
-  }
+    http_client_parameter apiHeaders[] = {
+        { header_str1, accessToken },
+        { header_str2, header_str3 },
+        { NULL, NULL }
+    };
+
+    lcd.print(status_inprogress);
+    delay(500);
+    http_client.debug(-1);
+    FILE* result = http_client.postURI(apiUri, NULL, apiData, apiHeaders);
+    int returnCode = http_client.getLastReturnCode();
+    lcd.clear();
+
+    if (result != NULL) {
+        http_client.closeStream(result);  // this is very important -- be sure to close the STREAM
+    } else {
+        lcd.print(status_failure);
+    }
+
+    if (returnCode == 201) {
+        lcd.print(status_data_uploaded);
+        lcd.setCursor(0, 1);
+        lcd.print(status_session_created);
+        return true;
+    } else {
+        lcd.print(status_error_code);
+        lcd.setCursor(0, 1);
+        lcd.print(returnCode);
+        return false;
+    }
 }
 
